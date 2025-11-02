d63599b6ebd37cb028ac5d6e0a68be05a983b320
johannvasquez05 <jvquezmadero@byupathway.edu>
2025-11-01 19:24:51 -0500

    C# Programming Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,48 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("whats your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"your grade is : {letter}");
+
+        if (percentage > 70)
+        {
+            Console.WriteLine("You Passed");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next time");
+        }
     }
 }
