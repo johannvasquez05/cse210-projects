6710d706927e9163f13cce428fd50b54ca9ebfa4
johannvasquez05 <jvquezmadero@byupathway.edu>
2025-11-15 21:36:54 -0500

    W03 Project Scripture Memorizer Program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,61 @@
+/*Added a list of scriptures to randomly display */
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Console.Clear();
+
+        List<Scripture> library = new List<Scripture>()
+        {
+            new Scripture(
+                new Reference("Proverbs", 3, 5, 6),
+                "Trust in the Lord with all thine heart; and lean not unto thine own understanding."
+            ),
+
+            new Scripture(
+                new Reference("John", 3, 16),
+                "For God so loved the world that he gave his only begotten Son, " +
+                "that whosoever believeth in him should not perish, but have everlasting life."
+            ),
+
+            new Scripture(
+                new Reference("Philippians", 4, 13),
+                "I can do all things through Christ which strengtheneth me."
+            ),
+
+            new Scripture(
+                new Reference("2 Nephi", 2, 25),
+                "Adam fell that men might be; and men are, that they might have joy."
+            ),
+
+            new Scripture(
+                new Reference("Moroni", 7, 47),
+                "Charity is the pure love of Christ, and it endureth forever."
+            )
+        };
+
+        Random rnd = new Random();
+        int index = rnd.Next(library.Count);
+        Scripture scripture = library[index];
+
+        while (!scripture.isCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press ENTER to hide words or type 'quit' to exit.");
+
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+                return;
+
+            scripture.HideRandomWords(3);
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine("All words are now hidden.");
+
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+        string[] splitWords = text.Split(" ");
+        foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = _random.Next(_words.Count);
+            _words[index].Hide();
+        }
+
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> resultWords = new List<string>();
+        foreach (Word word in _words)
+        {
+            resultWords.Add(word.GetDisplayText());
+        }
+
+        return $"{_reference.GetDisplayText()}\n{string.Join(" ", resultWords)}";
+    }
+
+    public bool isCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            return false;
+        }
+        return true;
+    }
+}
